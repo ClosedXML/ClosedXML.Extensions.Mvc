a6160c09840362903295d297136406b8e80fe87e
Francois Botha <igitur@gmail.com>
2017-06-05 15:26:32 +0200

    First commit

---
+++ src/ClosedXML.Extensions.Mvc/MvcExtensions.cs
@@ -0,0 +1,23 @@
+﻿using ClosedXML.Excel;
+using System.IO;
+using System.Web.Mvc;
+
+namespace ClosedXML.Extensions
+{
+    public static class MvcExtensions
+    {
+        public static FileStreamResult Deliver(this XLWorkbook workbook, string fileName, string contentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+        {
+            var memoryStream = new MemoryStream();
+
+            workbook.SaveAs(memoryStream);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return new FileStreamResult(memoryStream, contentType)
+            {
+                FileDownloadName = fileName
+            };
+
+        }
+    }
+}
